cb84380e0de97802ecd902d3d6158d9858d967b2
tiltaya <taialves95@gmail.com>
2022-11-07 22:31:20 -0300

    Exercícios aula objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+const sacolao = []
+
+sacolao.push({nome: "Maçã",
+    preco:  2.50.toFixed(2),
+    disponibilidade: true})
+
+sacolao.push({nome: "Banana",
+preco:  1.99.toFixed(2),
+disponibilidade: true})
+
+sacolao.push({nome: "Pêra",
+preco:  3.90.toFixed(2),
+disponibilidade: false})
+
+console.log(sacolao)
